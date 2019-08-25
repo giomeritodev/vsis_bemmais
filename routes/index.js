077fd4fb5e055042efab9a03f390aa5c9be3820e
giomerito <giomerito.souza@gmail.com>
2019-08-25 19:02:55 -0300

    Organizando conteudo com includes

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
 var conn = require('./../inc/db');
+var menus = require('./../inc/menus');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results) => {
-    if(err){
-      console.log(err);
-    }
+
+  menus.getMenus().then(results => {
+
     res.render('index', { 
       title: 'Restaurante Saboroso',
       menus: results 
     });
 
   });
+  
 });
 
 router.get('/contacts', function(req, res, next){
@@ -28,11 +27,19 @@ router.get('/contacts', function(req, res, next){
 });
 
 router.get('/menus', function(req, res, next){
-  res.render('menus', {
-    title: 'Menu - Restaurante Saboroso',
-    background: 'images/img_bg_4.jpg',
-    h1: 'Saboreie nosso menu!'
+
+  menus.getMenus().then(results => {
+
+    res.render('menus', {
+      title: 'Menu - Restaurante Saboroso',
+      background: 'images/img_bg_4.jpg',
+      h1: 'Saboreie nosso menu!',
+      menus: results
+    });
+    
   });
+
+  
 });
 
 router.get('/reservations', function(req, res, next){
