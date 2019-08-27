e451842f5a25cc7f0d03e839526c39ac406b57dc
giomerito <giomerito.souza@gmail.com>
2019-08-27 18:48:08 -0300

    Efetuando reserva com POST para o mysql

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var conn = require('./../inc/db');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var express = require('express');
 var router = express.Router();
 
@@ -44,11 +45,36 @@ router.get('/menus', function(req, res, next){
 });
 
 router.get('/reservations', function(req, res, next){
-  res.render('reservations', {
-    title: 'Reservas - Restaurante Saboroso',
-    background: 'images/img_bg_4.jpg',
-    h1: 'Reserve uma Mesa!'
-  });
+
+  reservations.render(req, res);
+  
+});
+
+router.post('/reservations', function(req, res, next){
+
+  if(!req.body.name){
+    reservations.render(req, res, "Informe um nome");
+  } else if(!req.body.email){
+    reservations.render(req, res, "Informe um email");
+  } else if(!req.body.email){
+    reservations.render(req, res, "Informe um email");
+  } else if(!req.body.people){
+    reservations.render(req, res, "Informe o número de pessoas");
+  } else if(!req.body.date){
+    reservations.render(req, res, "Informe uma data");
+  } else if(!req.body.time){
+    reservations.render(req, res, "Informe uma hora");
+  }  else{
+    reservations.save(req.body).then(results => {
+
+      req.body = {};
+
+      reservations.render(req, res, null, "Reserva adicionada com sucesso");  
+    }). catch(err => {
+      reservations.render(req, res, err.message);
+    });
+  }
+
 });
 
 router.get('/services', function(req, res, next){
